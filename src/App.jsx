b3fa15bb0c5e4e574b37030bfddbb34eb08dc77f
Muhammad Yaseen <Yaseen@Muhammads-MacBook-Pro.local>
2024-05-23 11:07:14 +0500

    work

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
             <Routes>
               <Route
                 path="/"
-                element={user ? <Navigate to="/profile" /> : <SignUp />}
+                element={user ? <Navigate to="/profile" /> : <SignIn />}
               />
               <Route path="/login" element={<SignIn />} />
                 <Route path="/register" element={<SignUp />} />
